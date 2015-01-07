e7d93cfebbeaba0ed50267a883e1a3bce6f50353
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-07 08:24:27 -0500

    Archivos de sitio en construcción

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>AIDIA MEIKER : Próximamente</title>
+
+<style type="text/css">
+body {
+	margin-top: 40px;
+	background-color: #dfdfdf;
+}
+</style>
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" marginwidth="0" data-feedly-mini="yes">
+<table width="788" border="0" align="center" cellpadding=" " cellspacing="0">
+  <tbody><tr>
+    <td><img src="img/index.jpg" width="788" height="480" alt=""></td>
+  </tr>
+</tbody></table>
+</body></html>
